debe2fc156152b3852a1f9570291bd17d2933062
Krzysztof-Kazmierczak <krzysztofkaz3@onet.eu>
2022-11-03 18:23:05 +0100

    Skończenie logowania i rejestracji.
    Do zarobienia:
     1) Dodanie Notyfikacji/Tokenu
     2) Stworzyć Ekran główny
      - Nazwa drużyny
      - Liczba zdobytych pkt
      - Liczba pieniędzy
      - Opis następnej kolejki
     3) Ekran z Naszą drużyną
      - Boisko z naszymi zawodnikami
      - Liczba pieniedzy
      - Przy kazdym zawodników ile zdobył pkt w poprzedniej kolejce
     4) Tworzenie składu
      - Kliknięcie na wolne pole wyświetla wszystkich zawodników z danej pozycji (tylko Br i z pola)
      - Ile kosztuje
      - Jaka drużyna
      - Kapitan i ViceKapitan
     5) Ekran z Informacjami
      - Jak pkt przyznawane
      - Ile z danej drużyny
      - "Specjalne karty" co dają jak używać

--- app/src/main/java/com/example/slpn/fragments/login/Login.kt
+++ app/src/main/java/com/example/slpn/fragments/login/Login.kt
@@ -1,57 +1,97 @@
 package com.example.slpn.fragments.login
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.slpn.R
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.slpn.databinding.FragmentLoginBinding
+import com.example.slpn.fragments.register.RegisterViewModel
+import com.example.slpn.fragments.repository.BaseFragment
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class LoginFragment : BaseFragment() {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Login.newInstance] factory method to
- * create an instance of this fragment.
- */
-class Login : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
+    private val fbAuth = FirebaseAuth.getInstance()
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+    private val regVm by viewModels<RegisterViewModel>()
+    private val LOG_DEUBG = "LOG_DEBUG"
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLoginBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupLoginClick()
+        setupRegistrationClick()
+        //Sprawdzanie połączenia z internetem
+        observeInternetConnection()
+    }
+    //Sprawdzanie połączenia z internetem
+    override fun onResume() {
+        super.onResume()
+        regVm.checkInternetConnection(requireActivity().application)
+    }
+    //Sprawdzanie połączenia z internetem
+    private fun observeInternetConnection(){
+        regVm.isConnectedToTheInternet.observe(viewLifecycleOwner){
+            it?.let{
+                binding.networkConnection.visibility = if(it) View.GONE else View.VISIBLE
+            }
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Login.
-         */
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Login().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    //Przejście do fragmentu z tworzeniem nowego użytkownika
+    private fun setupRegistrationClick() {
+        binding.RegisterButton.setOnClickListener {
+            findNavController()
+                .navigate(LoginFragmentDirections.actionLoginFragmentToRegisterFragment().actionId)
+        }
+    }
+    //Próba zalogowania do aplikacji
+    private fun setupLoginClick() {
+        binding.LoginButton.setOnClickListener {
+            //Pobranie informacji z pól tekstowych
+            val email = binding.LogMail.editText!!.text.toString().trim()
+            val pass = binding.LogPass.editText!!.text.toString().trim()
+            if(email.isNotEmpty() and pass.isNotBlank())
+            {
+                //Wysłanie do bazy danych czy login i hasło są prawidłowe
+                fbAuth.signInWithEmailAndPassword(email, pass)
+                    .addOnSuccessListener { authRes ->
+                        //Jeżeli wszystko się zgadza logujemy się do aplikacji
+                        if(authRes.user != null) startApp()
+                    }
+                    .addOnFailureListener{ exc ->
+                        //Jeżeli występuje błąd wyświetlamy komunikat o błędzie użytkownikowi
+                        Snackbar.make(requireView(), "Błędny login lub hasło", Snackbar.LENGTH_SHORT)
+                            .show()
+                        Log.d(LOG_DEUBG, exc.message.toString())
+                    }
+            }
+            else{
+                if(email.isEmpty()){
+                    binding.LogMail.helperText = "Field can't be empty"
+                }
+                if(pass.isEmpty()){
+                    binding.LogPass.helperText = "Field can't be empty"
                 }
             }
+
+        }
     }
 }
